a6c028335bb0c7a666e59e7f32d5d01ea26c557c
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-06-21 15:09:12 -0400

    adjustments to test server seeding

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const db = require('./config/connection');
+const express = require("express");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
