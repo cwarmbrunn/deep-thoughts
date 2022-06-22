bc2a8761574fe5b2a2935c30412b972b2d24c52d
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-06-22 12:35:22 -0400

    added concurrently and testing client and server folders visibility

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 
+const path = require("path");
 // Import Apollo Server
 const { ApolloServer } = require("apollo-server-express");
 
@@ -32,6 +33,14 @@ const startApolloServer = async (typeDefs, resolvers) => {
   // Integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
 };
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => {
