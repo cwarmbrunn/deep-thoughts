6db721a9b5537ae64fc5e473bbd53b68090361c1
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-06-25 14:10:31 -0400

    added in mutations.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,6 +6,8 @@ import Auth from "../utils/auth";
 import { useQuery } from "@apollo/client";
 import { QUERY_THOUGHTS, QUERY_ME_BASIC } from "../utils/queries";
 
+import ThoughtForm from "../components/ThoughtForm";
+
 const Home = () => {
   const { loading, data } = useQuery(QUERY_THOUGHTS);
   const { data: userData } = useQuery(QUERY_ME_BASIC);
@@ -16,25 +18,18 @@ const Home = () => {
   return (
     <main>
       <div className="flex-row justify-space-between">
-        <div className={`col-12 mb-3 ${loggedIn && "col-lg-8"}`}>
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <ThoughtList
-              thoughts={thoughts}
-              title="Some Feed for Thought(s)..."
-            />
-          )}
-        </div>
-        {loggedIn && userData ? (
-          <div className="col-12 col-lg-3 mb-3">
-            <FriendList
-              username={userData.me.username}
-              friendCount={userData.me.friendCount}
-              friends={userData.me.friends}
-            />
+        {loggedIn && (
+          <div className="col-12 mb-3">
+            <ThoughtForm />
           </div>
-        ) : null}
+        )}
+        <div className={`col-12 mb-3 ${loggedIn && "col-lg-8"}`}>
+          <FriendList
+            username={userData.me.username}
+            friendCount={userData.me.friendCount}
+            friends={userData.me.friends}
+          />
+        </div>
       </div>
     </main>
   );
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import { Navigate, useParams } from 'react-router-dom';
+import React from "react";
+import { Navigate, useParams } from "react-router-dom";
 
-import ThoughtList from '../components/ThoughtList';
-import FriendList from '../components/FriendList';
+import ThoughtList from "../components/ThoughtList";
+import FriendList from "../components/FriendList";
+import { QUERY_USER, QUERY_ME } from "../utils/queries";
+import Auth from "../utils/auth";
 
-import { useQuery } from '@apollo/client';
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
-import Auth from '../utils/auth';
+import { ADD_FRIEND } from "../utils/mutations";
+
+import { useQuery, useMutation } from "@apollo/client";
+
+import ThoughtForm from "../components/ThoughtForm";
 
 const Profile = (props) => {
+  const [addFriend] = useMutation(ADD_FRIEND);
   const { username: userParam } = useParams();
 
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
@@ -34,22 +39,31 @@ const Profile = (props) => {
       </h4>
     );
   }
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+          Viewing {userParam ? `${user.username}'s` : "your"} profile.
         </h2>
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
-        <div className="col-12 mb-3 col-lg-8">
-          <ThoughtList
-            thoughts={user.thoughts}
-            title={`${user.username}'s thoughts...`}
-          />
-        </div>
+        <div className="col-12 mb-3 col-lg-8"></div>
 
         <div className="col-12 col-lg-3 mb-3">
           <FriendList
